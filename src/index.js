8da81b2962b276b30cc52e55ecf3a1c706c0d012
Maurya <mauryapatel093@gmail.com>
2024-08-01 16:19:46 +0530

    add middlewares and custom responses

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+.then(
+    () =>{
+        app.listen(process.env.PORT || 8000)
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        app.on("error", (error) => {
+            console.log("Error connecting :",error);
+        })
+    }
+)
+.catch((err) => {
+    console.log("Mongo DB connection failed ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+        Approach 1
+
+import express from "express";
+const app = express();
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{ 
+            console.log("Error connnecting : ",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT , ()=>{
+            console.log(`App listening on ${process.env.PORT}` );
+        })
+    }catch(error){
+        console.error("Error : ",error);
+        throw error
+    }
+})()
+*/
